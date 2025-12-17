550d814d94eefad3b20939d6a6160073ab4b72e0
Sebastian Zamora <zam98qui@gmail.com>
2025-12-17 15:44:12 -0500

    Capitulo 9 completo - ajustes finales

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -8,14 +8,6 @@ import CardWrapper from '@/app/ui/dashboard/cards';
 import { LatestInvoicesSkeleton, RevenueChartSkeleton, CardsSkeleton } from '@/app/ui/skeletons';
  
 export default async function Page() {
-  const latestInvoices = await fetchLatestInvoices();
-  const {
-    numberOfCustomers,
-    numberOfInvoices,
-    totalPaidInvoices,
-    totalPendingInvoices
-  } = await fetchCardData();
-
   return (
     <main>
       <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
